94a2830a7b256a613ac9d9e242db0e0962ba53c9
indalkp <113878290+indalkp@users.noreply.github.com>
2026-04-12 06:46:29 +0530

    Add view.js file with error log messages

---
+++ src/drawing/view.js
@@ -0,0 +1,4 @@
+
+[log_consolidator.py] ModuleNotFoundError: No module named 'log_consolidator'
+[obs_logger_bridge.lua] [OBS Logger] Python path or logger script not configured
+[obs_logger_bridge.lua] [OBS Logger] Could not write chapter file: E:/Workspaces/Workspace/Project OBS/obs_logging_system/logs/ai_captions/scene_chapters_20260412_064528.txt
